bcb85f2b742bb6cdca6db6389348e9f0aaeac997
Nisha Madhavan <Nisha.Madhavan@quest.com>
2020-08-26 19:06:41 +0530

    Problem Statement 2- Business Rules Engine

---
+++ OrderProcessing/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing
+{
+    class Book:IOrder,ICommission
+    {
+        public string Name { get; set; }
+        public string OrderID { get; set; }
+        public int quantity { get; set; }
+        public float price { get; set; }
+
+        public void FetchOrders()
+        {
+            this.Name = "Book";
+            this.OrderID = "B1";
+            this.price = 500;
+            this.quantity = 2;
+
+        }
+
+        public float GetCommission()
+        {
+            return this.price * 2 / 100;
+
+        }
+
+        public void PrintOrders()
+        {
+            FetchOrders();
+            Console.WriteLine("This order is for {0} the order Id is {1} , price is {2}, quantity is {3}", this.Name, this.OrderID, this.price, this.quantity);
+        }
+
+        public string ProcessOrders()
+        {
+            float com = GetCommission();
+            PrintOrders();
+            return "Created order for the above physical product and calculated commission as " + com;
+        }
+    }
+}
---
+++ OrderProcessing/PhysicalProduct.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing
+{
+    class PhysicalProduct : IOrder,ICommission
+    {
+        public string Name { get ; set; }
+        public string OrderID { get ; set; }
+        public int quantity { get; set; }
+        public float price { get ; set ; }
+
+        public void FetchOrders()
+        {
+            this.Name = "Physical product";
+            this.OrderID = "P1";
+            this.price = 5000;
+            this.quantity = 3;
+
+        }
+
+        public float GetCommission()
+        {
+            return this.price * 5 / 100;
+            
+        }
+
+        public void PrintOrders()
+        {
+            FetchOrders();
+            Console.WriteLine("This order is for {0} the order Id is {1} , price is {2}, quantity is {3}", this.Name,this.OrderID,this.price,this.quantity);
+        }
+
+        public string ProcessOrders()
+        {
+            float com = GetCommission();
+            PrintOrders();
+            return "Created order for the above physical product and calculated commission as "+com;
+        }
+    }
+}
